0ce0f8c949b189b1327ef3085a1529f241d7d6ee
Jake Kirsch <jake.kirsch@gfs.com>
2025-05-13 15:11:42 -0400

    Proto - Update import for renderer in main.py

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import os
-from .renderer import render_color_music
+from app.renderer import render_color_music
 
 app = FastAPI()
 
