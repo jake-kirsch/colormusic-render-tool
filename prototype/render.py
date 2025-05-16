86b1cbec7bbf18e6f01d3a87d1520ee3b1469bfd
Jake Kirsch <jake.kirsch@gfs.com>
2025-05-16 15:43:15 -0400

    Proto - Accids management (cont)

--- prototype/render.py
+++ prototype/render.py
@@ -144,13 +144,12 @@ def label_notes(soup):
                     element_name = _element_name
                     break
         
+        accid_tracker_key = ":::".join([str(measure_num), pname.upper(), octave, ])
         if element_name == "accid":  # Visible-only
             # If accid_val is set need to propagate this through for a given note in the same measure in the same octave
-            accid_tracker_key = ":::".join([str(measure_num), pname.upper(), octave, ])
-            if accid_val:
-                accid_tracker[accid_tracker_key] = accid_val
-            else:
-                accid_val = accid_tracker.get(accid_tracker_key, accid_val)
+            accid_tracker[accid_tracker_key] = accid_val
+        elif not accid_val:
+            accid_val = accid_tracker.get(accid_tracker_key, accid_val)
 
         if accid_val == "s":
             # Sharp
